4e57d2cdcfa4e86a56cc1adb52f4d212c3dfddec
faris <faris38kanbur@gmail.com>
2020-12-11 16:48:46 +0100

    add controller temps

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 const AuthRouter = require("./AuthRouter");
+const ProfileRouter = require("./ProfileRouter");
+const BookRouter = require("./BookRouter");
 
 /** 
  * @route /api/auth
@@ -10,6 +12,22 @@ const AuthRouter = require("./AuthRouter");
 
 // Only /api endpoint
 router.use("/auth", AuthRouter);
+
+
+
+/** 
+ * @route /api/profile
+ * @desc Route for profile
+*/
+// router.use("/profile", ProfileRouter);
+
+
+
+/** 
+ * @route /api/book
+ * @desc Route for book
+*/
+// router.use("/book", BookRouter);
 
 
 module.exports = router;
