fbae72ebf70f4bd65cd57b46c4b595acf95dcb49
faris <faris38kanbur@gmail.com>
2020-12-16 15:11:19 +0100

    books page rechecked

--- routes/BookRouter.js
+++ routes/BookRouter.js
@@ -1,23 +1,21 @@
 const express = require("express");
 const router = express.Router();
-
 const BookController = require("../controllers/BookController");
 
+// base url: /api/books
 
 /**
- * @route GET/ api /books
- * @desc Book Listing endpoint
- * @access Public
+ * @route   GET /api/books
+ * @desc    Books Listing endpoint
+ * @access  Public
  */
-
- router.get("/", BookController.getBookList);
+router.get("/", BookController.getBookList);
 
 /**
- * @route GET/ api /books / details/:id
- * @desc Book Details endpoint
- * @access Public
+ * @route   GET /api/books/details/:id
+ * @desc    Books Details endpoint
+ * @access  Public
  */
+router.get("/details/:id", BookController.getBookDetails);
 
- router.get("/details/:id", BookController.getBookDetails)
-
- module.exports = router;
+module.exports = router;
