ee6e5197187c0880d6953a4af174eb77fe9635b2
faris <faris38kanbur@gmail.com>
2020-12-16 23:25:03 +0100

    add client again

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ connectDB();
 app.use(express.json());
 app.use("/api", router);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+});
+}
+
 app.listen(process.env.PORT, () => {
   console.log(`I'm listening on port ${process.env.PORT}`);
 });
