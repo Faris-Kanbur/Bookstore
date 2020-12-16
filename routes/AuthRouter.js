76f44be92311e952c0253ec70f0339a13de6b50b
faris <faris38kanbur@gmail.com>
2020-12-16 14:47:53 +0100

    Update Validations

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,10 +1,10 @@
-  
 const express = require("express");
-const router = express.Router();
-const {check} = require("express-validator");
-
-
+const { check } = require("express-validator");
 const AuthController = require("../controllers/AuthController");
+const validations = require("../middleware/validationMiddleware");
+
+const router = express.Router();
+
 // routes for /api/auth
 
 /**
@@ -13,33 +13,20 @@ const AuthController = require("../controllers/AuthController");
  * @access  Public
  */
 router.post(
-    "/register",
-[
-    check ("password", "Please enter a password with 6 and more chars").isLength(
-    {
-        min: 6,
-    }
-    ),
-    check("email", "Please enter a valid email").isEmail(),
-],
-
-AuthController.authRegister);
+  "/register",
+  validations.emailPasswordValidation,
+  AuthController.authRegister
+);
 
 /**
  * @route   POST /api/auth/login
  * @desc    Login endpoint
- * @access  Private
+ * @access  Public
  */
-router.post("/login",
-[
-    check("email", "Please enter a valid email").isEmail(),
-    check ("password", "Please enter a password with 6 and more chars").isLength(
-    {
-        min: 6,
-    }
-    ),
-],
-
-AuthController.authLogin);
+router.post(
+  "/login",
+  validations.emailPasswordValidation,
+  AuthController.authLogin
+);
 
 module.exports = router;
